c0c1d47dff58a0d7729d4ef03b20cd089fc547fb
msnsadq <nannatsadik@gmail.com>
2022-04-20 14:06:43 +0530

    added more feutures

--- lib/moviesuggestpage/moviesuggest.dart
+++ lib/moviesuggestpage/moviesuggest.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:providersapi/provider/searchmovieprovider.dart';
 import 'package:providersapi/secondpage.dart';
+import 'package:providersapi/tvDetailsPage/tvDetailPage.dart';
 
 class MovieSuggest extends StatefulWidget {
   const MovieSuggest({Key? key}) : super(key: key);
@@ -18,81 +19,90 @@ class _MovieSuggestState extends State<MovieSuggest> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: SingleChildScrollView(
-       // scrollDirection: Axis.vertical,
+        // scrollDirection: Axis.vertical,
         child: ConstrainedBox(
-          constraints: BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
-
-
+          constraints:
+              BoxConstraints(maxHeight: MediaQuery.of(context).size.height),
           child: Column(
-            children: [Padding(
-              padding: const EdgeInsets.only(left: 8,right: 8,top: 25),
-              child: Container(
-                color: Colors.white,
-                child: TextField(
-                  onChanged: (value){
-                    searchMovieProvider.searchMovieWork(context, value);
-                  },
-                  autofocus: true,
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white, width: 2.0),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white, width: 2.0),
-                    ),
-                    hintText: 'Which movie do you like?',hintStyle: TextStyle(color: Colors.black)
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8, right: 8, top: 25),
+                child: Container(
+                  color: Colors.white,
+                  child: TextField(
+                    onChanged: (value) {
+                      searchMovieProvider.searchMovieWork(context, value);
+                    },
+                    autofocus: true,
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 2.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.white, width: 2.0),
+                        ),
+                        hintText: 'Which movie do you like?',
+                        hintStyle: TextStyle(color: Colors.black)),
                   ),
                 ),
               ),
-            ),
               SizedBox(
                 height: 10,
               ),
               Align(
                   alignment: Alignment.bottomLeft,
-                  child: Text("Recent",style: TextStyle(color: Colors.white),)),
-              searchMovieProvider.loading? Center(child: CircularProgressIndicator(),):Expanded(
-                child: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      mainAxisSpacing: 5,
-                      crossAxisSpacing: 5,
-                    ),
-                    itemCount:
-                    searchMovieProvider.searchMovieModel.results!.length,
-                    itemBuilder: (ctx, index) {
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (ctx) => ScreenTwo(
-                                  id: searchMovieProvider
-                                      .searchMovieModel.results![index].id
-                                      .toString())));
-                        },
-                        child: searchMovieProvider.searchMovieModel
-                            .results![index].posterPath !=
-                            null
-                            ? Card(
-                          elevation: 20,
-                          shadowColor: Colors.red,
-                          child: Image.network(
-                            imageurl +
-                                searchMovieProvider.searchMovieModel
-                                    .results![index].posterPath
-                                    .toString(),
-                            fit: BoxFit.fill,
+                  child: Text(
+                    "Recent",
+                    style: TextStyle(color: Colors.white),
+                  )),
+              searchMovieProvider.loading
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : Expanded(
+                      child: GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 1,
+                            mainAxisSpacing: 5,
+                            crossAxisSpacing: 5,
                           ),
-                        )
-                            : SizedBox(),
-                      );
-                    }),
-              ),
-            ],
+                          itemCount: searchMovieProvider
+                              .searchMovieModel.results!.length,
+                          itemBuilder: (ctx, index) {
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) => TvDetailPage(
+                                          id: searchMovieProvider.searchMovieModel
+                                              .results![index].id
+                                              .toString(),
+                                        )));
+                              },
+                              // child:
+                              child: Container(
+                                color: Colors.red,
+                                height: 150,
+                                child: Row(
+                                  children: [
+                                    Card(
 
+                                      child: Image.network(imageurl+ searchMovieProvider.searchMovieModel.results![index].posterPath.toString(),fit: BoxFit.fill,),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+
+                          }),
+
+                    ),
+            ],
           ),
         ),
       ),
-
     );
   }
 }
